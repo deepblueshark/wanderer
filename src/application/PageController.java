995b2494dbb469420a28d4871b2f1f10a32b9cec
deepblueshark <khcy5ckk@nottingham.edu.my>
2017-02-16 12:23:03 +0800

    clean up not done, experimenting

--- src/application/PageController.java
+++ src/application/PageController.java
@@ -1,18 +1,43 @@
 package application;
 
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import javax.imageio.ImageIO;
+
+import com.neet.DiamondHunter.MapViewer.TileInformation;
+import com.sun.javafx.logging.Logger;
+
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 
 public class PageController extends Application{
+	
+	Canvas c1;
+	
+	double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
 	
 	final static int CANVAS_WIDTH = 796;
     final static int CANVAS_HEIGHT = 427;
@@ -22,13 +47,11 @@ public class PageController extends Application{
     @Override
     public void start(final Stage primaryStage) {
  
-        final Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+        Canvas canvas = c1;
         final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        initDraw(graphicsContext);
+        //initDraw(graphicsContext);
          
-        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
-                new EventHandler<MouseEvent>(){
- 
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent event) {
                 graphicsContext.beginPath();
@@ -37,8 +60,9 @@ public class PageController extends Application{
                 graphicsContext.stroke();
             }
         });
-
-	 public void handle(ActionEvent t) {
+    
+    @FXML
+    public void saveFile(ActionEvent t) {
          FileChooser fileChooser = new FileChooser();
           
          //Set extension filter
@@ -52,7 +76,7 @@ public class PageController extends Application{
          if(file != null){
              try {
                  WritableImage writableImage = new WritableImage(CANVAS_WIDTH, CANVAS_HEIGHT);
-                 canvas.snapshot(null, writableImage);
+                 c1.snapshot(null, writableImage);
                  RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
                  ImageIO.write(renderedImage, "png", file);
              } catch (IOException ex) {
@@ -60,7 +84,20 @@ public class PageController extends Application{
              }
          }
      }
-      
- });
+
+    }
+ 
+ 	 @FXML
+ 	public void drag(MouseEvent t) {
+         double offsetX = t.getSceneX() - orgSceneX;
+         double offsetY = t.getSceneY() - orgSceneY;
+         double newTranslateX = orgTranslateX + offsetX;
+         double newTranslateY = orgTranslateY + offsetY;
+          
+         ((ImageView)(t.getSource())).setTranslateX(newTranslateX);
+         ((ImageView)(t.getSource())).setTranslateY(newTranslateY);
+     }
+ 	 
+ 	 
 	
 }
